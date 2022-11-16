b2bdea180d9b138982e16cbc8057a1d282acdf11
alexxey7 <78179999+alexxey7@users.noreply.github.com>
2022-11-16 16:09:57 +0200

    Added event listeners, sounds, background imgs

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document
+        .querySelectorAll("button")
+        [i].addEventListener("click", HandleClick);
+}
+
+function HandleClick() {
+    let audio;
+    switch (this.innerHTML) {
+        case "w":
+            audio = new Audio("sounds/tom-1.mp3");
+            break;
+        case "a":
+            audio = new Audio("sounds/tom-2.mp3");
+            break;
+        case "s":
+            audio = new Audio("sounds/tom-3.mp3");
+            break;
+        case "d":
+            audio = new Audio("sounds/tom-4.mp3");
+            break;
+        case "j":
+            audio = new Audio("sounds/crash.mp3");
+            break;
+        case "k":
+            audio = new Audio("sounds/kick-bass.mp3");
+            break;
+        case "l":
+            audio = new Audio("sounds/snare.mp3");
+            break;
+        default:
+            alert("YEAH BABYY");
+            break;
+    }
+    audio.play();
+}
