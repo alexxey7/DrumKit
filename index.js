b91fb3aa719573e8c71066a0fe9af7064d33f177
alexxey7 <78179999+alexxey7@users.noreply.github.com>
2022-11-16 16:48:03 +0200

    Sounds are now played when key is pressed

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 "use strict";
 
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document
         .querySelectorAll("button")
-        [i].addEventListener("click", HandleClick);
+        [i].addEventListener("click", function () {
+            var buttonInnerHTML = this.innerHTML;
+
+            MakeSound(buttonInnerHTML);
+        });
 }
 
-function HandleClick() {
-    let audio;
-    switch (this.innerHTML) {
+addEventListener("keydown", function (event) {
+    MakeSound(event.key);
+});
+
+function MakeSound(key) {
+    var audio;
+    switch (key) {
         case "w":
             audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
             break;
         case "a":
             audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
             break;
         case "s":
             audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
             break;
         case "d":
             audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
             break;
         case "j":
             audio = new Audio("sounds/crash.mp3");
+            audio.play();
             break;
         case "k":
             audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
             break;
         case "l":
             audio = new Audio("sounds/snare.mp3");
+            audio.play();
             break;
+        case "u":
+            audio = new Audio("sounds/ukraine.mp3");
+            audio.play();
         default:
-            alert("YEAH BABYY");
             break;
     }
-    audio.play();
 }
